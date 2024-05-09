f44e0b339f2901c7a9e5cd0e847af7445c8b8d75
Niknuby <165725277+Niknuby@users.noreply.github.com>
2024-05-09 22:12:51 +0100

    add client server

---
+++ dictionary.py
@@ -0,0 +1,4 @@
+import json
+
+data = {'key': 'value'}
+serialized_data = json.dumps(data)  # Serialize dictionary to JSON string
---
+++ text.py
@@ -0,0 +1,4 @@
+with open('file.txt', 'r') as f:
+    file_contents = f.read()
+
+# Now, send file_contents to the server using sockets.
